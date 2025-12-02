4be31bcec9cde7a6dc5fa15a932b3e72188ac22a
David Shaw <dshaw11@utk.edu>
2025-12-02 10:33:36 -0500

    setting up day2

--- 2025/day2/day2.py
+++ 2025/day2/day2.py
@@ -1,20 +1,18 @@
 
 
 def main():
-     filename = "day1.txt"
-    # filename = "practice1.txt"
-    instructions = []
-
-    dial = 50
-
-    password = 0
+    #  filename = "day2.txt"
+    filename = "practice2.txt"
+    productId = []
 
     try:
         with open(filename, "r") as f:
             lines = f.readlines()
             for line in lines:
-                instructions.append(line.strip())
+                productId.append(line.strip())
     except FileNotFoundError:
         print(f"Error the file '{filename}' was not found.")
     except Exception as e:
         print(f"An error occurred: {e}")
+
+    
