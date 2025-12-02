0a7f80d2f925394a886a015473c707b78278ba2d
David Shaw <dshaw11@utk.edu>
2025-12-02 09:45:04 -0500

    sliced array to split instruction apart into distance and direction

--- 2025/day1.py
+++ 2025/day1.py
@@ -1,15 +1,31 @@
 
 def main():
-    filename = "day1.txt"
+    # filename = "day1.txt"
+    filename = "practice1.txt"
+    instructions = []
+
+    dial = 50
+
     try:
         with open(filename, "r") as f:
             lines = f.readlines()
             for line in lines:
-                print(line.strip())
+                instructions.append(line.strip())
     except FileNotFoundError:
         print(f"Error the file '{filename}' was not found.")
     except Exception as e:
         print(f"An error occurred: {e}")
 
+    
+    for i in instructions:
+        direction, distance = split_instruction(i)
+        print(f"direction: {direction} distance: {distance}")
+
+
+def split_instruction(instruction):
+    direction = instruction[0]
+    distance = instruction[1:]
+
+    return direction, distance
 
 main()
