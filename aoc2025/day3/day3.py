37c7022a69dd15c59325846e296ef4fa3eaca719
David Shaw <dshaw11@utk.edu>
2025-12-03 09:36:13 -0500

    i think this is as far as i can get on day3 part 2

--- aoc2025/day3/day3.py
+++ aoc2025/day3/day3.py
@@ -1,8 +1,8 @@
 
 
 def main() -> None:
-    # filename = "input.txt"
-    filename: str = "input.txt"
+    # filename:str = "input.txt"
+    filename: str = "sample-input2.txt"
     input_list:list = read_input(filename)
     
     output: int = 0
@@ -10,16 +10,35 @@ def main() -> None:
     for row in input_list:
         output += (find_largest(list(row)))
 
-    print(output)
+    # print(output)
     
 def find_largest(row: list):
     largest: int = 0
+    smallest_three_count:int = 3
+    count_dict:dict = {}
+    output:list = []
+    # dump count of characters into a dictionary
+    for i in range(0, len(row)):
+        count_dict[row[i]] = count_dict.get(row[i], 0) + 1
+
+    count_dict = dict(sorted(count_dict.items()))
+
+    for key, _ in count_dict.items():
+        if smallest_three_count == 0:
+            break
+
+        while smallest_three_count > 0:
+            if key in count_dict and count_dict[key] > 0:
+                count_dict[key] -= 1
+                smallest_three_count -= 1
+
 
     for i in range(0, len(row)):
-        for j in range(i, len(row)):
-            if(int(row[i] + row[j]) > largest and j != i):
-                largest = int(row[i] + row[j])
+        if row[i] in count_dict and count_dict[row[i]] > 0:
+            output.append(row[i])
+            count_dict[row[i]] -= 1
 
+    print(output)
     return largest
 
 def read_input(filename: str) -> list:
