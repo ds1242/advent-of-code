182baea4933844adb1ada632e2f69825429f7406
David Shaw <dshaw11@utk.edu>
2025-12-04 09:08:43 -0500

    finished part 1 of day 4

--- aoc2025/day4/day4.py
+++ aoc2025/day4/day4.py
@@ -1,30 +1,50 @@
 
 
 def day4():
-    # filename = "input.txt"
-    filename:str = "sample-input.txt"
+    filename:str = "input.txt"
+    # filename:str = "sample-input.txt"
     input_list:list = read_input(filename)
 
 
-    
+    available_rolls:int = 0
     for i in range(0, len(input_list)):
         for j in range(0, len(input_list[i])):
-            print(input_list[i][j])
+            # print(input_list[i][j])
             # set count to 0
-            count:int = 0
-            
-            # check row above
+            curr_count:int = 0
 
-            # check left
+            # up and left
+            if input_list[i][j] == '@':
+                if i > 0 and j > 0 and input_list[i - 1][j - 1] == '@':
+                    curr_count += 1
+                # above
+                if i > 0 and input_list[i - 1][j] == '@':
+                    curr_count += 1
+                # up and right
+                if i > 0 and j < len(input_list[i]) - 1 and input_list[i - 1][j + 1] == '@':
+                    curr_count += 1
+                # left
+                if j > 0 and input_list[i][j - 1] == '@': 
+                    curr_count += 1
+                # right
+                if j < len(input_list[i]) - 1 and input_list[i][j + 1] == '@':
+                    curr_count += 1
+                # below and left
+                if i < len(input_list[i]) - 1 and j > 0 and input_list[i + 1][j - 1] == '@':
+                    curr_count += 1
+                # below
+                if i < len(input_list[i]) - 1 and input_list[i + 1][j] == '@':
+                    curr_count += 1
+                # below and right
+                if i < len(input_list[i]) - 1 and j < len(input_list[i]) - 1 and input_list[i + 1][j + 1] == '@':
+                    curr_count += 1
 
-            # check right
+                if curr_count < 4:
+                    available_rolls += 1
 
-            # check row below
-    #     for j in range(0, len(input_list[i])):
-    #         matrix[i][j] = input_list[i][j]
-    
-    # print(matrix[0][3])
+            # print(f"found at i:{i} j:{j}")
 
+    print(available_rolls)
 
 def read_input(filename: str) -> list:
     inputList = []
