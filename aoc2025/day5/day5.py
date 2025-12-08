387b03577579dca2b8b12281a6ea6d4f7ad795da
David Shaw <dshaw11@utk.edu>
2025-12-08 08:04:10 -0500

    working range check function

--- aoc2025/day5/day5.py
+++ aoc2025/day5/day5.py
@@ -4,7 +4,17 @@ def day5() -> None:
     filename:str = "sample-input.txt"
     input_list:str = read_input(filename)
 
-    print(input_list.split('\n\n'))
+    input_list = input_list.split()
+    input_range: list = []
+    value_list: list = []
+    for row in input_list:
+        if '-' in row:
+            input_range.append(row)
+        else:
+            value_list.append(row)
+
+    print(input_range)
+    print(value_list)
 
 
 def read_input(filename: str) -> str:
@@ -19,6 +29,9 @@ def read_input(filename: str) -> str:
 
     return productIDs
 
+
+def split_value(input: str) -> tuple[int, int]:
+    
 
 day5()
 
