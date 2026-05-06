638bb72ec4b175b32bc4aff2958a8e2ec20b6697
David Shaw <dshaw11@utk.edu>
2026-05-06 14:48:30 -0400

    added second array to check direction

--- lcs/lcs.py
+++ lcs/lcs.py
@@ -2,22 +2,28 @@
 X = ['A', 'B', 'C', 'B', 'D', 'A', 'B']
 Y = ['B', 'D', 'C', 'A', 'B', 'A']
 
-def LCS_Length(x:list, y: list) -> list:
-    c = [[0] * (len(y)+1)] * (len(x)+1)
+def LCS_Length(x:list, y: list):
+    c = [[0]*(len(y)+1) for _ in range(len(x) + 1)]
+    b = [[""]*(len(y)+1) for _ in range(len(x) + 1)]
     print(c)
-    for i in range(1, len(x)):
-        for j in range(1, len(y)):
+    for i in range(0, len(x)):
+        for j in range(0, len(y)):
+            #print(f"x = %d, y = %d", x[i], y[j])
             if x[i] == y[j]:
                 c[i][j] = c[i - 1][j - 1] + 1
+                b[i][j] = "NW"
             elif c[i - 1][j] >= c[i][j - 1]:
                 c[i][j] = c[i - 1][j]
+                b[i][j] = "N"
             else:
                 c[i][j] = c[i][j - 1]
+                b[i][j] = "W"
 
-    return c
+    return c, b
 
    
 
-Z = LCS_Length(X, Y)
+Z, B = LCS_Length(X, Y)
 print(Z)
+# print(B)
 
