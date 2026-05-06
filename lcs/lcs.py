ab6acdd5af1b577da41bb82c397e2cbd0d74d226
David Shaw <dshaw11@utk.edu>
2026-05-06 15:29:40 -0400

    finished printing longest common subsequence

--- lcs/lcs.py
+++ lcs/lcs.py
@@ -3,13 +3,14 @@ X = ['A', 'B', 'C', 'B', 'D', 'A', 'B']
 Y = ['B', 'D', 'C', 'A', 'B', 'A']
 
 def LCS_Length(x:list, y: list):
-    c = [[0]*(len(y)+1) for _ in range(len(x) + 1)]
-    b = [[""]*(len(y)+1) for _ in range(len(x) + 1)]
-    print(c)
-    for i in range(0, len(x)):
-        for j in range(0, len(y)):
+    m = len(x)
+    n = len(y)
+    c = [[0 for _ in range(n + 1)] for _ in range(m + 1)]
+    b = [["" for _ in range(n + 1)] for _ in range(m + 1)]
+    for i in range(1, m + 1):
+        for j in range(1, n + 1):
             #print(f"x = %d, y = %d", x[i], y[j])
-            if x[i] == y[j]:
+            if x[i - 1] == y[j - 1]:
                 c[i][j] = c[i - 1][j - 1] + 1
                 b[i][j] = "NW"
             elif c[i - 1][j] >= c[i][j - 1]:
@@ -18,12 +19,24 @@ def LCS_Length(x:list, y: list):
             else:
                 c[i][j] = c[i][j - 1]
                 b[i][j] = "W"
-
-    return c, b
+    print(b)
+    Print_LCS(b, x, m, n)
 
    
+def Print_LCS(b, X, i, j):
+    if i == 0 or j == 0:
+        return
 
-Z, B = LCS_Length(X, Y)
-print(Z)
-# print(B)
+    if b[i][j] == "NW":
+        Print_LCS(b, X, i - 1, j - 1)
+        print(X[i - 1])
+    elif b[i][j] == "N":
+        Print_LCS(b, X, i - 1, j)
+    else:
+        Print_LCS(b, X, i, j - 1)
+
+
+LCS_Length(X, Y)
+#print(B)
+
 
